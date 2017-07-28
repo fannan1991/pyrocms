9e84ea6b14af5117eb18850eb5074df3ef4f4ffa
fannan <fannan1991@gmail.com>
2017-07-28 17:51:57 +0800

    '20170728'

--- addons/shared/fannan/members-module/resources/config/permissions.php
+++ addons/shared/fannan/members-module/resources/config/permissions.php
@@ -11,5 +11,29 @@ return [
         'write',
         'delete'
     ],
-
+    'gold'  => [
+        'read',
+        'write',
+        'delete'
+    ],
+    'loan'  => [
+        'read',
+        'write',
+        'delete'
+    ],
+    'repayment'  => [
+        'read',
+        'write',
+        'delete'
+    ],
+    'withdraw'  => [
+        'read',
+        'write',
+        'delete'
+    ],
+    'message'  => [
+        'read',
+        'write',
+        'delete'
+    ],
 ];
